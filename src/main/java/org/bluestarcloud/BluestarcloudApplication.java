0f9365a2833e9ca4ddd4749be4ef7029cb0cdcbf
swapdani <99826448+swapdani@users.noreply.github.com>
2025-08-14 13:44:49 +0530

    Initial commit

---
+++ src/main/java/org/bluestarcloud/BluestarcloudApplication.java
@@ -0,0 +1,13 @@
+package org.bluestarcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluestarcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluestarcloudApplication.class, args);
+	}
+
+}
